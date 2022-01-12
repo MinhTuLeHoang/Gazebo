e5f7ed4faeb44496fc202a3f9688405cc304ef9e
Minh Tú Lê Hoàng <90244396+MinhTuLeHoang@users.noreply.github.com>
2022-01-12 17:30:30 +0700

    not done

--- code python/action_controller.py
+++ code python/action_controller.py
@@ -9,7 +9,8 @@ from rosgraph_msgs.msg import Clock
 from beginner_tutorials.msg import Num
 
 # Reading .xml file
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
+from lxml import etree
 
 
 ##############################################
@@ -17,7 +18,7 @@ import xml.etree.ElementTree as ET
 ##   This file is made by Minh Tu           ##
 ##   |----------------------------|         ##
 ##   |version: 1.0.0              |         ##
-##   |Update date: 10-01-2022     |         ##
+##   |Update date: 12-01-2022     |         ##
 ##   |----------------------------|         ##
 ##   |Description:                |         ##
 ##   |   - Depend gazebo clock    |         ##
@@ -26,49 +27,138 @@ import xml.etree.ElementTree as ET
 ##                                          ##
 ##############################################
 
-rospy.init_node("action_topic", disable_signals=True)
+
 pub = rospy.Publisher("/cmd_vel",Twist, queue_size=5)
 vel_cmd = Twist()
+
+tree = etree.parse('/home/minhtulehoang/my_ws/src/beginner_tutorials/src/rules.xml')
+root = tree.getroot()
+
+def func_on_shutdown():
+	vel_cmd.linear.x = 0
+	vel_cmd.angular.z = 0
+	pub.publish(vel_cmd)
+rospy.on_shutdown(func_on_shutdown)
+
+def Initial_System():
+	print("Get started")
+	print("Please wait . . .")
+	t = 0
+	while(t == 0):
+		t = rospy.get_time()
+	print("Starting complete")
+	print("=================")
+
+
+
+
+# Some function
+##################
+
+def most_frequent(List):
+	return max(set(List), key = List.count)
+
+
+def stop():
+	vel_cmd.linear.x = 0
+	vel_cmd.angular.z = 0
+	pub.publish(vel_cmd)
 
 LIDAR = 0
 SIGN = 2
 LANE = 1
 
 FREE = 0
+BUSY = 1
 
 
 state = FREE
 
 last_sign_time = 0
-last_lane_time = 0
+sign_arr = []
+temp =root.xpath("./SIGN")
+SIGN_ARR_SIZE = temp.attrib["arr_size"]
+SIGN_ARR_ALIVE_TIME = temp.attrib["alive_time"]
+
+
+
+class MyCar:
+	class Speed:
+		linear = 0
+		angular = 0
+	speed = Speed()
+myCar = MyCar()
+	
 
 def callbackFunction(msg):
 	print("okela")
 	global state
-
-	global last_lane_time
+	global myCar
 	global last_sign_time
+
+	global sign_arr
+
 
 	if(msg.header == LIDAR):
 		print("I'm lidar msg")
 	elif(msg.header == SIGN):
-		sgin_id = msg.base_arg
-		# root = ET.parse("rules.xml").getroot()
-		# rules_arr = root.find("")
-		print("I'm traffic sign")
+		print("I see ", end="")
+		print(msg.base_arg)
+
+		if not sign_arr:	#sign_arr is empty
+			last_sign_time = rospy.get_time()
+		else:
+			current_time = rospy.get_time()
+			if(current_time - last_sign_time >= SIGN_ARR_ALIVE_TIME):
+				sign_arr = []
+				last_sign_time = rospy.get_time()
+
+		
+		sign_arr.append(msg.base_arg)
+		if(len(sign_arr) >= SIGN_ARR_SIZE):
+			sign_arr = []
+			state = BUSY
+			sign_id = most_frequent(sign_arr)
+
+			global root
+			rules = root.xpath("./SIGN/rule[@sign_id=$temp]", temp=sign_id)
+
+			current_rule = ""
+			for rule in rules:
+				condition = rule.find("condition").text
+				if(eval(condition)):
+					current_rule = rule
+					break
+
+			action = current_rule.find("action").attrib["name"]
+			eval(action + "()")
+		
+		# print("I'm traffic sign")
 	elif(state == FREE and msg.header == LANE):
+		print("=========================")
+		print("Run automatic with: ")
+		myCar.speed.linear = msg.float_1
+		myCar.speed.angular = msg.float_2
+		print("   V_linear = ", end="")
+		print(myCar.speed.linear)
+		print("   V_angular = ", end="")
+		print(myCar.speed.angular)
+
 		vel_cmd.linear.x = msg.float_1
 		vel_cmd.angular.z = msg.float_2
-		print("I'm lane detec")
+		pub.publish(vel_cmd)
+
 	# else:
 	# 	print("something wrong happens")
 
 
 
-# From now on
-##############
+# From now on MAIN function
+#############################
+Initial_System()
 
 while not rospy.is_shutdown():
 	print("---------------begin---------------")
+	rospy.init_node("action_topic", disable_signals=True)
 	lis = rospy.Subscriber("/controller", Num, callbackFunction)
 	rospy.spin()
